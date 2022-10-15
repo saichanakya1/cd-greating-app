2d680ff6f68f326b3dabe8893458db094831de0d
Sai Chanakya <Saichanakya1@gmail.com>
2022-10-15 20:38:05 +0000

    Intial version of the Clickatell POC Greeting DevOps app

---
+++ src/main/java/com/clickatell/poc/greeting/demo/GithubActionCdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.clickatell.poc.greeting.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionCdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionCdDemoApplication.class, args);
+	}
+
+}
