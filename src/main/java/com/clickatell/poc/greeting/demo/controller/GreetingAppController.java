3cb9427a3ddbc41b7f09ee81a2c2ac04dee54302
Sai Chanakya <Saichanakya1@gmail.com>
2022-10-16 09:04:20 +0000

    Unit test cases are added

--- src/main/java/com/clickatell/poc/greeting/demo/controller/GreetingAppController.java
+++ src/main/java/com/clickatell/poc/greeting/demo/controller/GreetingAppController.java
@@ -3,12 +3,20 @@ package com.clickatell.poc.greeting.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class GreetingAppController {
 
     @RequestMapping("/")
     public String greeting(){
 	return "Spring is here!";
+    }
+
+
+    @RequestMapping("/health")
+    public String health(){
+        return "HEALTH CHECK OK!";
     }
 
 }
