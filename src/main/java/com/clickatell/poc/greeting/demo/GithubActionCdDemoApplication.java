f8eacec2cd31daa0f1c6f965d7c2499864b05cd7
Sai Chanakya <Saichanakya1@gmail.com>
2022-10-16 19:41:20 +0000

    Modified GithubActionCdDemoApplication to cover sonar quebe

--- src/main/java/com/clickatell/poc/greeting/demo/GithubActionCdDemoApplication.java
+++ src/main/java/com/clickatell/poc/greeting/demo/GithubActionCdDemoApplication.java
@@ -1,5 +1,6 @@
 package com.clickatell.poc.greeting.demo;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
