6577068fc5d03aa5dd8a04a60e4608bf513eb668
Sai Chanakya <Saichanakya1@gmail.com>
2022-10-16 19:33:40 +0000

    SAST testing update for main funcation

--- src/test/java/com/clickatell/poc/greeting/demo/GithubActionCdDemoApplicationTests.java
+++ src/test/java/com/clickatell/poc/greeting/demo/GithubActionCdDemoApplicationTests.java
@@ -2,12 +2,18 @@ package com.clickatell.poc.greeting.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class GithubActionCdDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    GithubActionCdDemoApplication githubActionCdDemoApplication;
+
+    @Test
+    void contextLoads() {
+        assertThat(githubActionCdDemoApplication).isNotNull();;
+    }
 
 }
